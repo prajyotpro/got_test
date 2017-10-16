30ab8a10c02c193e68ed55b8bbb9314d25cdc993
prajyot <prajyot.khandeparkar@bidchat.com>
2017-10-16 19:46:29 +0530

    first commit

--- app/routes/user.js
+++
@@ -1,7 +0,0 @@
-var User        = require('../controllers/user');
-var express 		= require('express');
-var router 			= express.Router();
-
-router.get('/', User.getUsers);
-
-module.exports = router;
